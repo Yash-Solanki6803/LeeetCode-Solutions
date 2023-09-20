432868ee8746cf43748c8ad1325db73d090040a7
Yash <thisisyashs@gmail.com>
2023-09-20 16:48:45 +0530

    Practice.js Testing

--- Practice.js
+++ Practice.js
@@ -1,3 +1,5 @@
-let arr = [[1,2,3],[4,5,6]];
+// let arr = [[1,2,3],[4,5,6]];
 
-console.log(arr[0][1]);
+// console.log(arr[0][1]);
+
+// Code entered by Yash Solanki's Practice account for testing
