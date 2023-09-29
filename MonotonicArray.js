cc93c1cbc5df1f55a4a4aefa045a789dce38a9c5
Yash <thisisyashs@gmail.com>
2023-09-29 20:11:56 +0530

    by Yash Solanki

--- MonotonicArray.js
+++ MonotonicArray.js
@@ -1,26 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var isMonotonic = function (nums) {
-  let increasing;
-  let flag = true;
-  increasing = nums[0] < nums[1] ? true : false;
-  console.log("increasing", increasing);
-  //   nums.map((prev, i) => {
-  //     console.log(prev, nums[i + 1], prev <= nums[i + 1]);
-  //     if (prev < nums[i + 1] !== increasing && i < nums.length - 1) flag = false;
-  //   });
+  if (nums.length < 2) return true;
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    console.log(nums[i], nums[i + 1], nums[i] < nums[i + 1]);
-    if (
-      nums[i] < nums[i + 1] !== increasing &&
-      i < nums.length - 1 &&
-      nums[i] !== nums[i + 1]
-    )
-      flag = false;
+  let check = 0; // 0 means unknown, 1 means increasing, -1 means decreasing
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      // increasing
+      if (check == 0) check = 1;
+      else if (check == -1) return false;
+    } else if (nums[i] < nums[i - 1]) {
+      // decreasing
+      if (check == 0) check = -1;
+      else if (check == 1) return false;
+    }
   }
 
-  return flag;
+  return true;
 };
-
-let nums = [6, 5, 4, 4];
-
-console.log(isMonotonic(nums));
