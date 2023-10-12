b94491a8fc6dfc1534851365472594c818e3b9ca
Yash <thisisyashs@gmail.com>
2023-10-12 14:48:33 +0530

    by Yash Solanki

---
+++ CacheWithTimeLimit.js
@@ -0,0 +1,41 @@
+var TimeLimitedCache = function () {
+  this.cache = new Map(); // Using Map so we don't need a size variable
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  let found = this.cache.has(key);
+  if (found) clearTimeout(this.cache.get(key).ref); // Cancel previous timeout
+  this.cache.set(key, {
+    value, // Equivalent to `value: value`
+    ref: setTimeout(() => this.cache.delete(key), duration),
+  });
+  return found;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+  return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
